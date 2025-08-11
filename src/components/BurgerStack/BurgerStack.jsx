17eadaef49d3aab584a57a6018f50c4242a5d139
GenioZx <eli.rodri14@email.com>
2025-08-11 10:20:17 -0400

    lab completed

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,19 @@
+// src/components/BurgerStack/BurgerStack.jsx
+
+const BurgerStack = ({ ingredients, removeFromBurger }) => {
+  return (
+    <ul>
+      {ingredients.map((ingredient, index) => (
+        <li 
+          key={index} 
+          style={{ backgroundColor: ingredient.color }}
+        >
+          {ingredient.name}
+          <button onClick={() => removeFromBurger(index)}>X</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default BurgerStack;
